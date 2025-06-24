7a15d228aabd730efaa6093ab8e8ef2dae2badce
Soumyashree Samantaray <142867348+Soumyashree1234@users.noreply.github.com>
2025-06-24 21:48:12 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Builtin_filters
 def Builtin_filters(request):
     import datetime
     d={'data':'this is a builtin filters class','dt':datetime.datetime.now,'c':5}
     return render(request,'Builtin_filters.html',d) 
+
+#Userdefined_Filters
+def Userdefined_Filters(request):
+    import datetime
+    d={'data':'this is a builtin filters class'}
+    return render(request,'Userdefined_Filters.html',d) 
